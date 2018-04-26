9110d3a51de72aff72b5682e5b74a3c347fe6ebc
Seth Tisue <seth@tisue.net>
2018-04-26 15:31:55 +0200

    scala-xml is no longer bundled

--- src/main/scala/scaladist/Main.scala
+++ src/main/scala/scaladist/Main.scala
@@ -4,7 +4,6 @@ import scala.util.Properties.versionString
 
 object Main extends App {
   println(s"scala.util.Properties.versionString: $versionString")
-  XmlTest.test()
   ScalapTest.test()
   ReflectTest.test()
 }
